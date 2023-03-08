631f1b2082a967ae32a5e04815e0e6b2306b9b72
Diogo L Scarmagnani <diogols@live.com>
2023-03-08 14:16:06 -0300

    exercicio 3

--- 07_mar/script.js
+++ 07_mar/script.js
@@ -1,123 +1,247 @@
+class Carro {
+    #marca;
+    #modelo;
+    #ano;
+    #cor;
+    #quilometragem;
+    #valorFipe;
+
+    constructor(marca, modelo, ano, cor, quilometragem, valorFipe) {
+        this.#marca = marca;
+        this.#modelo = modelo;
+        this.#ano = +ano;
+        this.#cor = cor;
+        this.#quilometragem = +quilometragem;
+        this.#valorFipe = +valorFipe;
+    }
+
+    /* Os setters foram comentados pois não são necessários. */
+    
+    get marca() { return this.#marca; }
+    // set marca(value) { this.#marca = value; }
+
+    get modelo() { return this.#modelo; }
+    // set modelo(value) { this.#modelo = value; }
+
+    get ano() {return this.#ano; }
+    // set ano(value) { this.#ano = +value; }
+
+    get cor() { return this.#cor; }
+    // set cor(value) { this.#cor = value; }
+
+    get quilometragem() {return  this.#quilometragem; }
+    // set quilometragem(value) { this.#quilometragem = +value; }
+
+    get valorFipe() { return this.#valorFipe; }
+    // set valorFipe(value) { this.#valorFipe = +value; }
+    
+    anosUtilizacao() {
+        let anoAtual = new Date().getFullYear();
+        return anoAtual - this.#ano;
+    }
+
+    #valorMercado(kmPorAno) {
+        if (kmPorAno <= 30000)
+            return this.#valorFipe * 1.1;
+
+        if (kmPorAno > 30000 && kmPorAno <= 50000)
+            return this.#valorFipe;
+
+        return this.#valorFipe * 0.9;
+    }
+
+    valorMercado() {
+        let kmPorAno = this.#quilometragem / this.anosUtilizacao();
+        return this.#valorMercado(kmPorAno);
+    }
+}
+
+String.prototype.ucFirst = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 function getValuesDoForm(form) {
     let dados = new FormData(form);
     
     let newObj = {};
     for (const [name, val] of dados) {
-        newObj[name] = val;
+        newObj[name.replace(/-/g, '_')] = val;
     }
 
     return newObj;
 }
 
-function compareValues(a, b, asc) {
-    if (!isNaN(a))
-        a = +a;
-
-    if (!isNaN(b))
-        b = +b;
-
-    if (asc === true)
-        return (a < b) ? -1 : (a > b) ? 1 : 0;
-    return (a > b) ? -1 : (a < b) ? 1 : 0;
+function compareValues(a, b) {
+    if (a < b)
+        return -1;
+    
+    if (a > b)
+        return 1;
+    
+    return 0;
 }
 
-function cleanAllThAttributes(allTh, cellIndex, cellIndexException = -1) {
-    try {
-        for (let i = 0; i < allTh.length; i++) {
-            if (i != cellIndexException) {
-                const iFontAwesome = allTh[i].querySelector("i");
-
-                if (iFontAwesome !== null)
-                    iFontAwesome.remove();
-
-                if (i != cellIndex)
-                    allTh[i].removeAttribute("data-order-by");
-            }
-        }
-        return true;
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-function sortTableByColumn(thElement) {
-    const theadElement = thElement.closest("thead");
-    const allTh = theadElement.querySelectorAll("tr > th");
-    let cellIndex = thElement.cellIndex;
-    let colNum = cellIndex + 1;
-    let asc = allTh[cellIndex].toggleAttribute("data-order-by");
-
-    let allThCleaned = cleanAllThAttributes(allTh, cellIndex, 1);
-    if (allThCleaned !== true)
+function sortOrderedList(olElement) {
+    if (olElement.children.length == 0)
         return;
     
-    const tableTbody = theadElement.nextElementSibling;
+    let allLi = [...olElement.querySelectorAll("li")];
     
-    let rows = [...tableTbody.querySelectorAll("tr")];
-    let qs = `td:nth-child(${colNum})`;
-    
-    rows.sort( (r1, r2) => {
-        let t1 = r1.querySelector(qs);
-        let t1TextContent = t1.textContent.trim().toLowerCase();
-        let t2 = r2.querySelector(qs);
-        let t2TextContent = t2.textContent.trim().toLowerCase();
+    allLi.sort( (li1, li2) => {
+        let t1TextContent = li1.innerHTML.trim().toLowerCase();
+        let t2TextContent = li2.innerHTML.trim().toLowerCase();
         
-        return compareValues(t1TextContent, t2TextContent, asc);
+        return compareValues(t1TextContent, t2TextContent);
     });
     
-    rows.forEach(row => tableTbody.append(row));
-    
-    let iContent = asc === true ? `<i class="fa-solid fa-arrow-up-short-wide"></i>` : `<i class="fa-solid fa-arrow-down-short-wide"></i>`;
-    allTh[cellIndex].innerHTML += iContent;
+    allLi.forEach(li => olElement.append(li));
 }
 
-function sortTableByColumn(thElement) {
-    let cellIndex = thElement.cellIndex;
-    const theadElement = thElement.closest("thead");
-    const allTh = theadElement.querySelectorAll("tr > th");
-    let asc = allTh[cellIndex].toggleAttribute("data-order-by");
+function adicionarCarroSimples(values) {
+    let marca = values.marca_simples;
+    let modelo = values.modelo_simples;
 
-    for (let i = 0; i < allTh.length; i++) {
-        if (i != 1) {
-            const iFontAwesome = allTh[i].querySelector("i");
+    const details = document.querySelector("#details-" + modelo);
+    const ol = details.lastElementChild;
 
-            if (iFontAwesome !== null)
-                iFontAwesome.remove();
+    ol.insertAdjacentHTML("beforeend", `<li>${marca}</li>`);
 
-            if (i != cellIndex)
-                allTh[i].removeAttribute("data-order-by");
-        }
+    if (details.classList.contains("d-none"))
+        details.classList.remove("d-none");
+
+    if (details.closest("div").classList.contains("d-none")) {
+        details.closest("div").classList.remove("d-none");
+        details.closest("div").previousElementSibling.classList.add("d-none");
     }
-    
-    Utils.sortTableByColumn(theadElement, cellIndex, asc);
+
+    sortOrderedList(ol);
+
+    closeAllDetails();
+    details.open = true;
 }
 
-function verificarPalindromo(e) {
+function aparecerDetalhesCarro(carro) {
+    const divDetalhes = document.querySelector("#div-detalhes");
+    const dlDetalhes = divDetalhes.querySelector("#dl-detalhes");
+    const ddMarca = dlDetalhes.querySelector("#dd-marca");
+    const ddModelo = dlDetalhes.querySelector("#dd-modelo");
+    const ddAno = dlDetalhes.querySelector("#dd-ano");
+    const ddCor = dlDetalhes.querySelector("#dd-cor");
+    const ddKm = dlDetalhes.querySelector("#dd-quilometragem");
+    const ddValorFipe = dlDetalhes.querySelector("#dd-valor-fipe");
+    const ddAnosUtilizacao = dlDetalhes.querySelector("#dd-anos-utilizacao");
+    const ddValorMercado = dlDetalhes.querySelector("#dd-valor-mercado");
+
+    ddMarca.innerHTML = carro.marca.ucFirst();
+    ddModelo.innerHTML = carro.modelo.ucFirst();
+    ddAno.innerHTML = carro.ano;
+    ddCor.innerHTML = carro.cor.ucFirst();
+    ddKm.innerHTML = carro.quilometragem.toLocaleString("pt-BR");
+    ddValorFipe.innerHTML = carro.valorFipe.toLocaleString("pt-BR", {style: 'currency', currency: 'BRL'});
+    ddAnosUtilizacao.innerHTML = carro.anosUtilizacao();
+    ddValorMercado.innerHTML = carro.valorMercado().toLocaleString("pt-BR", {style: 'currency', currency: 'BRL'});
+
+    divDetalhes.previousElementSibling.classList.add("d-none");
+    divDetalhes.classList.remove("d-none");
+}
+
+function adicionarCarroDetalhado(values) {
+    let marca = values.marca_detalhado;
+    let modelo = values.modelo_detalhado;
+    let ano = values.ano_detalhado;
+    let cor = values.cor_detalhado;
+    let quilometragem = values.quilometragem_detalhado;
+    let valorFipe = values.valor_fipe_detalhado;
+
+    const carro = new Carro(marca, modelo, ano, cor, quilometragem, valorFipe);
+
+    aparecerDetalhesCarro(carro);
+}
+
+function adicionarCarro(e) {
     e.preventDefault();
-
     let values = getValuesDoForm(e.target);
-    let valorLower =
-        values.palindromo.trim() // Remove possíveis espaços no início e no fim da string.
-            .normalize('NFD') // Normaliza o texto no Formato de Normalização Canônico de Decomposição (acho desnecessário mas ok).
-            .replace(/[^\wÀ-ÖØ-öø-ÿ]/g, '') // Remove caracteres especiais, como pontos, traços e até mesmo espaços.
-            .replace(/[\u0300-\u036f]/g, '') // Substitui letras acentuadas por letras normais.
-            .toLowerCase(); // Deixa todas as letras em caixa baixa.
 
-    let valorParaCompara = reverseString(valorLower);
+    if (e.target.id == "form-carro-simples") {
+        adicionarCarroSimples(values);
+        return;
+    }
 
-    let textAlert = valorLower == valorParaCompara ? "É um palíndromo." : "Não é um palíndromo.";
-
-    alert(textAlert);
+    adicionarCarroDetalhado(values);
 }
 
-const iniciarAtualizaHora = () => setInterval(atualizaHora, 1000);
+function closeAllDetails() {
+    const allDetails = [...document.querySelectorAll("details:not(.d-none):has(> summary.list-items)")];
+    
+    allDetails.forEach((detail) => {
+        detail.open = false;
+    });
+}
+
+function limparCarrosSimples() {
+    const divCarros = document.querySelector("#div-carros-simples");
+    const allDetails = [...divCarros.querySelectorAll("details")];
+    
+    allDetails.forEach((detail) => {
+        detail.classList.add("d-none");
+        detail.querySelector("ol").innerHTML = '';
+    });
+
+    divCarros.classList.add("d-none");
+    divCarros.previousElementSibling.classList.remove("d-none");
+}
+
+function limparCarrosDetalhado() {
+    const divDetalhes = document.querySelector("#div-detalhes");
+    const allDd = [...divDetalhes.firstElementChild.querySelectorAll("dd")];
+    
+    allDd.forEach( (dd) => dd.innerHTML = '' );
+    divDetalhes.classList.add("d-none");
+    divDetalhes.previousElementSibling.classList.remove("d-none");
+}
+
+function limparCarros(e) {
+    if (e.target.id == "form-carro-simples") {
+        limparCarrosSimples();
+        return;
+    }
+
+    limparCarrosDetalhado();
+}
+
+function mudarCorTextoResumo(fechouTodos = false) {
+    if (fechouTodos === true) {
+        document.querySelector("div#texto-resumo").classList.remove("color-grey");
+        return;
+    }
+
+    document.querySelector("div#texto-resumo").classList.add("color-grey");
+}
+
+function fecharOutroDetail(idAberto) {
+    let strSelector = idAberto == "exercicio-1" ? "details#exercicio-2" : "details#exercicio-1";
+    document.querySelector(strSelector).open = false;
+}
+
+function clicouDetailPrincipal(e) {
+    if (e.target.closest("details").open === true) {
+        mudarCorTextoResumo(true);
+        return;
+    }
+
+    let id = e.target.closest("details").id;
+    fecharOutroDetail(id);
+    mudarCorTextoResumo();
+}
 
 async function rodarQuandoCarregar() {
-    await diaHoje();
-    await iniciarAtualizaHora();
+    console.log("Entrou.");
 }
 
-document.querySelector("#form-carro").addEventListener("submit", verificarPalindromo);
+document.querySelector("#form-carro-simples").addEventListener("submit", adicionarCarro);
+document.querySelector("#form-carro-simples").addEventListener("reset", limparCarros);
+document.querySelector("#form-carro-detalhado").addEventListener("submit", adicionarCarro);
+document.querySelector("#form-carro-detalhado").addEventListener("reset", limparCarros);
+document.querySelectorAll("div.principal > details > summary").forEach( item => item.addEventListener("click", clicouDetailPrincipal) );
 document.querySelector("#btn-voltar").addEventListener("click", () => location = "../index.html");
-// document.addEventListener("DOMContentLoaded", rodarQuandoCarregar);
-document.addEventListener("DOMContentLoaded", () => location = "../index.html");
+document.addEventListener("DOMContentLoaded", rodarQuandoCarregar);
